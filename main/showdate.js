33ee22af887b10e5ec3257547d61ae3adae63e4f
aerotica0512 <minata@foxmail.com>
2023-12-26 20:12:56 +0800

    20231226

--- main/showdate.js
+++ main/showdate.js
@@ -2,8 +2,8 @@ function showDate() {
     today = new Date();
 
     year = today.getYear()+1900;
-    month = today.getMonth();
-    day = today.getDay();
+    month = today.getMonth()+1;
+    day = today.getDay()+24;
 
     document.write(year+"年"+month+"月"+day+"日");
 }
